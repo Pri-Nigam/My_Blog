56437072293c05a1d659786c3fed2a83583b2361
Pri-Nigam <prinigam@bestpeers.com>
2023-06-07 18:51:25 +0530

    Added New and Create action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,13 +2,28 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+
   def show
-    # byebug
     @article = Article.find(params[:id])
   end
+
+  def new
+    @article = Article.new 
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to @article
+    else
+      render :new
+    end
+  end
+
   def edit
     @article = Article.find(params[:id])
   end
+
   def update
     @article = Article.find(params[:id])
     if @article.update(article_params)
@@ -17,7 +32,9 @@ class ArticlesController < ApplicationController
       render :edit
     end
   end
+
   private
+
   def article_params
     params.require(:article).permit(:title, :body)
   end
