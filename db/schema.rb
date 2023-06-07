7c58089553764d342f658763857caba8145523ba
Pri-Nigam <prinigam@bestpeers.com>
2023-06-07 13:47:57 +0530

    Removed email from articles

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_07_074924) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_07_080939) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "email"
   end
 
 end
